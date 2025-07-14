6e25c9aecb50e27967b70796accee616c05a87dd
Dzmitry Skavarodka <skovdimka@gmail.com>
2025-07-14 18:15:24 +0200

    initial commit

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import { BuildOptions } from "./types/config";
+import webpack from "webpack";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolves } from "./buildResolves";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(option: BuildOptions): webpack.Configuration {
+    const {paths, mode, isDev} = option;
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(option),
+        module: {
+            rules: buildLoaders(option)
+        },
+        resolve: buildResolves(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(option) : undefined,
+    };
+}

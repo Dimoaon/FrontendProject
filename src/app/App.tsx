9d2df8315887c16373a26f3a976f41ebbed6f435
Dzmitry Skavarodka <skovdimka@gmail.com>
2025-07-19 22:03:23 +0200

    create navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,16 +6,16 @@ import { useTheme } from './providers/ThemeProvider'
 import { AboutPage } from 'pages/AboutPage'
 import { MainPage } from 'pages/MainPage'
 import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to='/'>Main</Link>
-            <Link to='/about'>About</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     )
 }

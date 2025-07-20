13e1690aa111948d80c6db18ce70b78c6572e8e0
Dzmitry Skavarodka <skovdimka@gmail.com>
2025-07-20 20:28:29 +0200

    implement i18n (translation)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,24 @@
-import React, { Suspense, useContext, useState } from 'react'
+import React, { Suspense} from 'react'
 import './styles/index.scss'
-import { Link, Route, Routes } from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from './providers/ThemeProvider'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
+
 
 const App = () => {
     const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className='content-page'>
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback="">
+                <Navbar />
+                <div className='content-page'>
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     )
 }
